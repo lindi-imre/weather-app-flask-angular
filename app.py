a7b8eacf45494649f9c3ceeca6381c49e54c76e1
imre.lindi <imre.lindi@codecool.com>
2022-05-25 08:16:15 +0200

    login works from website it navigates to the dashboard when success

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from functools import wraps
+
 import jwt
 from flask import Flask, jsonify, request
 from datetime import timedelta, datetime
@@ -9,7 +11,26 @@ app.config['SECRET_KEY'] = 'VERYSECRETKEY'
 CORS(app)
 
 
+def token_required(func):
+    # decorator factory which invoks update_wrapper() method and passes decorated function as an argument
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('token')
+        if not token:
+            return jsonify({'Alert!': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            print(data)
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+        except:
+           return jsonify({'Message': 'Invalid token'}), 403
+        return func(*args, **kwargs)
+    return decorated
+
 @app.route('/')
+@token_required
 def hello_world():  # put application's code here
     return 'Hello World!'
 
@@ -24,7 +45,6 @@ def loginJwt():
         return jsonify({"token": token.encode().decode("UTF-8")})
 
     return jsonify({"message" : "invalid login credentials"})
-
 
 if __name__ == '__main__':
     app.run()
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private client:HttpClient, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  doLogin(username: string, password: string) {
+    this.client.post<any>('http://localhost:5000/loginjwt', {username: username, password: password}).subscribe(resp =>{
+      if(resp.message) {
+        alert(resp.message);
+      }
+      else {
+        localStorage.setItem('token', resp.token);
+        this.router.navigate(['dashboard']);
+      }
+    })
   }
 
 }
